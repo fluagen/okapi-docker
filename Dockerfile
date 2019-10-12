717bfbe21e96aee384aff0252ca40ab8d1334a9e
fluagen <nakupendacc@gmail.com>
2019-10-12 11:12:31 +0800

    实现可以定制的环境

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
 FROM fluagen/ubuntu-java8
 
-# deploy okapi
-
-COPY ./repo/ /app/repo/
-COPY ./init.d /app/init.d
-COPY ./start.sh /app/start.sh
-
-RUN chmod -R 777 /app/init.d/* && \ 
-	chmod 777 /app/start.sh
 
 WORKDIR /app
 
-# start okapi service
+COPY start.sh /app/start.sh
+RUN chmod 777 /app/start.sh
+
 
 EXPOSE 9130
 
-ENTRYPOINT ["./start.sh"]
+CMD ["./start.sh"]
+
+#CMD ["/bin/bash","-c","while true;do sleep 1;done"]
--- start.sh
+++ start.sh
@@ -1,46 +1,126 @@
 #!/bin/bash
-set -e
 
-export tenant="testlib"
-export mod_permissions_version="mod-permissions-5.3.0-SNAPSHOT"
-export mod_login_version="mod-login-4.1.0-SNAPSHOT"
-export mod_users_version="mod-users-15.1.0-SNAPSHOT"
-export mod_users_bl_version="mod-users-bl-3.0.0-SNAPSHOT"
-export mod_authtoken_version="mod-authtoken-1.5.1-SNAPSHOT"
+# 等待数据库启动
+sleep 5;
 
-sleep 3
-echo 
-echo "start up okapi service"
-
-postgres_db_init=0
-
-if [[ "initdatabase" == $1 ]]; then
-	postgres_db_init=1
-fi
-
-
-exec java -jar \
+# 部署 okapi
+java -jar \
  -Dstorage=postgres \
  -Dpostgres_host=postgres \
- -Dpostgres_db_init=$postgres_db_init \
- /app/repo/okapi-core-fat.jar dev > okapi.log &
+ -Dpostgres_db_init=0 \
+ /app/repo/okapi/okapi-core-fat.jar dev > okapi.log &
 
 while ! echo exit | nc localhost 9130; do
 	sleep 1;
 done
 
-echo 
-echo "start deploy modules"
 
-source /app/init.d/tenant/init-tenant.sh
-source /app/init.d/modules/mod-permissions/deploy.sh
-source /app/init.d/modules/mod-login/deploy.sh
-source /app/init.d/modules/mod-users/deploy.sh
-source /app/init.d/modules/mod-users-bl/deploy.sh
-source /app/init.d/modules/mod-authtoken/deploy.sh
 
-echo 
-echo "finish deploy modules"
+# 部署依赖的 modules
+required_mods=$(cat repo/required/mods)
+
+for mod in $required_mods
+do
+     curl -X POST -s \
+          -H "Content-type:application/json" \
+          -d @/app/repo/required/$mod/ModuleDescriptor.json \
+          http://localhost:9130/_/proxy/modules
+
+     curl -X POST -s \
+          -H "Content-type:application/json" \
+          -d @/app/repo/required/$mod/DeploymentDescriptor.json \
+          http://localhost:9130/_/discovery/modules
+done
+
+ 
+# 初始化 tenant
+
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -d @/app/init.d/tenant.json \
+     http://localhost:9130/_/proxy/tenants
+
+for mod in $required_mods
+do
+     id=`cat repo/required/$mod/id`
+     curl -X POST -s \
+          -H "Content-type:application/json" \
+          -d "{\"id\":\"$id\"}" \
+          http://localhost:9130/_/proxy/tenants/testlib/modules
+done
+
+# 初始化 admin user
+
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -H "X-Okapi-Tenant:testlib" \
+     -d @/app/init.d/users.json \
+     http://localhost:9130/users
+
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -H "X-Okapi-Tenant:testlib" \
+     -d @/app/init.d/credential.json \
+     http://localhost:9130/authn/credentials
+
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -H "X-Okapi-Tenant:testlib" \
+     -d @/app/init.d/permissions.json \
+     http://localhost:9130/perms/users
+
+
+# 部署 mod-authtoken
+
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -d @/app/repo/mod-authtoken/ModuleDescriptor.json \
+     http://localhost:9130/_/proxy/modules
+
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -d @/app/repo/mod-authtoken/DeploymentDescriptor.json \
+     http://localhost:9130/_/discovery/modules
+
+id=`cat repo/mod-authtoken/id`
+curl -X POST -s \
+     -H "Content-type:application/json" \
+     -d "{\"id\":\"$id\"}" \
+     http://localhost:9130/_/proxy/tenants/testlib/modules
+
+##
+# 基础环境 完成部署
+##
+
+# 部署 非必备modules
+other_mods=$(cat repo/other/mods)
+
+for mod in $other_mods
+do
+     curl -X POST -s \
+          -H "Content-type:application/json" \
+          -d @/app/repo/other/$mod/ModuleDescriptor.json \
+          http://localhost:9130/_/proxy/modules
+
+     curl -X POST -s \
+          -H "Content-type:application/json" \
+          -d @/app/repo/other/$mod/DeploymentDescriptor.json \
+          http://localhost:9130/_/discovery/modules
+done
+
+for mod in $other_mods
+do
+     id=`cat repo/other/$mod/id`
+     curl -X POST -s \
+          -H "Content-type:application/json" \
+          -d "{\"id\":\"$id\"}" \
+          http://localhost:9130/_/proxy/tenants/testlib/modules
+done
+
+##
+# 非基础环境 完成部署
+##
+
 
 while true; do
 	sleep 1000;
